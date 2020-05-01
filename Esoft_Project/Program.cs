1fbd4d515486d7f0a5fc156325ce53e0ecd129f4
илья кстров <ilya.ilya-kostrov2015@yandex.ru>
2020-05-01 15:41:50 +0300

    test1

---
+++ Esoft_Project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esoft_Project
+{
+    static class Program
+    {
+        //создание статистического экземпляра класса модели ADO.EDM
+        public static WFTutorialEntities1 wftDB = new WFTutorialEntities1();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Menu());
+        }
+    }
+}
